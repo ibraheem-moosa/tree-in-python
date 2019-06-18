ae64ae78091ce685dfac3409e0132776c680df54
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2019-06-18 21:12:10 +0600

    Need to handle last child specially

--- tree.py
+++ tree.py
@@ -1,4 +1,19 @@
 from pathlib import Path
+from itertools import repeat
+
+def prefix():
+    yield '├──'
+    yield from repeat('│  ')
 
 def tree(path):
     yield path.name
+    if path.is_dir():
+        children = path.iterdir()
+        for child in children:
+            prefix_generator = prefix()
+            for line in tree(child):
+                yield next(prefix_generator) + line
+
+
+for line in tree(Path.cwd()):
+    print(line)
