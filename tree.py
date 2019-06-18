4b6a8d623354ec4448e293ee034a1a2f81864642
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2019-06-18 21:41:36 +0600

    Last child handling done

--- tree.py
+++ tree.py
@@ -1,18 +1,39 @@
 from pathlib import Path
 from itertools import repeat
 
-def prefix():
-    yield '├──'
-    yield from repeat('│  ')
+
+def not_last_prefix():
+    yield '├── '
+    yield from repeat('│   ')
+
+
+def last_prefix():
+    yield '└── '
+    yield from repeat('    ')
+
+
+def get_child_lines(child, prefix_generator):
+    for line in tree(child):
+        yield next(prefix_generator) + line
+
 
 def tree(path):
     yield path.name
     if path.is_dir():
         children = path.iterdir()
-        for child in children:
-            prefix_generator = prefix()
-            for line in tree(child):
-                yield next(prefix_generator) + line
+        try:
+            child = next(children)
+        except StopIteration:
+            # No child
+            return
+        try:
+            while True:
+                next_child = next(children)
+                yield from get_child_lines(child, not_last_prefix())
+                child = next_child
+        except StopIteration:
+            # Currently at last child
+            yield from get_child_lines(child, last_prefix())
 
 
 for line in tree(Path.cwd()):
